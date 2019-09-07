cacb324bbd08d298006f07cac2c0115f6b0e737f
Mark Jan van Kampen <mjvk@allseas.com>
2019-09-07 18:08:45 +0200

    Updates to 1.89 and make more in line with bincrafters

--- conanfile.py
+++ conanfile.py
@@ -1,36 +1,31 @@
 import os
 from conans import ConanFile, CMake, tools
 
-
 class CppCheckConan(ConanFile):
     name = "cppcheck_installer"
-    version = "1.88"
-    url = "https://github.com/bincrafters/conan-protoc_installer"
+    version = "1.89"
+    description = ("CppCheck is a linter for C++ that can catch various errors.")
+    topics = ("Cpp Check", "static analyzer", "linter")
+    url = "https://github.com/mjvk/conan-cppcheck_installer"
     homepage = "https://github.com/danmar/cppcheck/"
-    topics = ("Cpp Check", "static analyzer")
-    author = "mjvk <>"
-    description = ("flatc is a compiler for flatbuffers schema files. It can "
-                   "generate among others C++, Java and Python code.")
-    license = "BSD-3-Clause"
-    exports = ["LICENSE.txt"]
+    author = "Bincrafters <bincrafters@gmail.com>"
+    license = "GPL-3.0-or-later"
+    exports = ["LICENSE"]
     exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
-    settings = "compiler", "arch", "os_build", "arch_build"
-    #short_paths = True
+    
+    settings = "compiler", "os_build", "arch_build"
+    
     _source_subfolder = "source_subfolder"
     _build_subfolder = "build_subfolder"
 
     def source(self):
-        sha256 = "4aace0420d6aaa900b84b3329c5173c2294e251d2e24d8cba6e38254333dde3f"
+        sha256 = "37452d378825c7bd78116b4d7073df795fa732207d371ad5348287f811755783"
         tools.get("{0}/archive/{1}.tar.gz".format(self.homepage, self.version), sha256=sha256)
         os.rename("cppcheck-%s" % self.version, self._source_subfolder)
 
     def _configure_cmake(self):
         cmake = CMake(self)
-        cmake.definitions["FLATBUFFERS_BUILD_TESTS"] = False
-        cmake.definitions["FLATBUFFERS_BUILD_FLATLIB"] = False
-        cmake.definitions["FLATBUFFERS_BUILD_FLATHASH"] = False
-        cmake.definitions["FLATBUFFERS_INSTALL"] = True
         cmake.configure(build_folder=self._build_subfolder)
         return cmake
 
@@ -39,15 +34,15 @@ class CppCheckConan(ConanFile):
         cmake.build()
 
     def package(self):
-        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        self.copy("COPYING", dst="licenses", src=self._source_subfolder)
         self.copy("*", dst=os.path.join("bin","cfg"), src=os.path.join(self._source_subfolder,"cfg"))
         cmake = self._configure_cmake()
         cmake.install()
 
     def package_id(self):
         del self.info.settings.compiler
-        del self.info.settings.arch
-        self.info.include_build_settings()
 
     def package_info(self):
-        self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
+        bindir = os.path.join(self.package_folder, "bin")
+        self.output.info("Appending PATH environment variable: {}".format(bindir))
+        self.env_info.PATH.append(bindir)
